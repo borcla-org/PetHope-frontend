8e9435ac3ad18a5daad6277762528da77edad7bc
Abel <clarosabel2@gmail.com>
2025-11-27 11:25:47 -0300

    add images pets

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -5,7 +5,7 @@ import Image from 'next/image';
 export default function layout({ children }: LayoutProps) {
   return (
     <div className='bg-primary flex h-screen items-center justify-center gap-2'>
-      <Image className='absolute -bottom-30' src={"/animals/dog-cat.webp"} alt='dog and cat' width={550} height={550}></Image>
+      <Image className='absolute -bottom-33' src={"/animals/pets.png"} alt='dog and cat' width={1000} height={550}></Image>
       <div className=''>
         <Image src="/logo/pethope-logo-large.png" alt='logo' width={500} height={500}></Image>
       </div>
